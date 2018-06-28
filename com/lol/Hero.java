5e2adb74cdab681376514a522b5093be407dcbf7
r-paneva <32147137+r-paneva@users.noreply.github.com>
2018-06-28 10:49:44 +0300

    Whitout enum defences

--- com/lol/Hero.java
+++ com/lol/Hero.java
@@ -3,10 +3,23 @@ package com.lol;
 import java.util.ArrayList;
 import java.util.List;
 
-class Hero {
+public class Hero {
 
+    static String playerID;
     static String name;
-    static int health, mana, gold, attackDamage, abilityPower;
-    static List<String> defense = new ArrayList<String>();
+    static int health, mana, gold, attackDamage, abilityPower, normalDefence, magicDefence;
+    private static final int MAX_PLAYER_ID = 100;
+    private static final int MIN_PLAYER_ID = 3;
+
+    String getPlyerID() {
+        return playerID;
+    }
+
+    public void setPlyerID(String plyerID) {
+        if (playerID.length() < MIN_PLAYER_ID || playerID.length() > MAX_PLAYER_ID) {
+            return;
+        }
+        this.playerID = playerID;
+    }
 
 }
--- com/lol/Main.java
+++ com/lol/Main.java
@@ -3,13 +3,12 @@ package com.lol;
 public class Main {
 
     public static void main(String[] args) {
-        Hero rumy = new Caitlyn();
-        System.out.println(rumy.name+" health "+rumy.health+" abiityPower "+rumy.abilityPower);
-        System.out.println("attack damage:"+rumy.attackDamage+" mana:"+rumy.mana+" health:"+rumy.health);
-        System.out.println(rumy.defense);
-        Hero alex = new Lucian();
-        System.out.println(alex.name+" health "+alex.health+" abiityPower "+alex.abilityPower);
-        System.out.println("attack damage:"+alex.attackDamage+" mana:"+alex.mana+" health:"+alex.health);
-        System.out.println(alex.defense);
+        Hero rumy = new Caitlyn("Rumi");
+        System.out.println(rumy.getPlyerID() + " " + rumy.name + " health " + rumy.health + " abiityPower " + rumy.abilityPower);
+        System.out.println("attack damage:" + rumy.attackDamage + " mana:" + rumy.mana + " health:" + rumy.health);
+        Hero alex = new Lucian("Alex");
+        System.out.println(alex.name + " health " + alex.health + " abiityPower " + alex.abilityPower);
+        System.out.println("attack damage:" + alex.attackDamage + " mana:" + alex.mana + " health:" + alex.health);
+
     }
 }
